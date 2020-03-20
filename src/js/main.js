e378d52ddaca8dde2e2ce3035ecf4962bfedfb66
lukas <lukas3197@gmail.com>
2020-03-20 15:56:02 +0100

    feat: responsive navbar, page status

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 
-let menuList,navigationOpener, pageNext, pageBack, actualPage;
+let menuList,navigationOpener, pageNext, pageBack, pages, pageIndicator;
 let pageNumber = 0;
-let pageContents = ['header-content', 'process-content', 'portfolio-content', 'contact-content', 'footer-content'];
+const pageContentClass = '.pageContent';
+const pageIndicatorClass = '.bar-item';
+
 document.addEventListener("DOMContentLoaded", function() {
     //navigation
     menuList = document.getElementById("navigationList");
@@ -18,17 +20,33 @@ document.addEventListener("DOMContentLoaded", function() {
     //page switcher
     pageBack = document.getElementById('pageBack');
     pageNext = document.getElementById('pageNext');
+    pages = document.querySelectorAll(pageContentClass);
+    pageIndicator = document.querySelectorAll(pageIndicatorClass);
 
     pageNext.addEventListener('click', function () {
-        console.log(pageNumber);
-        document.getElementById(pageContents[pageNumber]).classList.add('hide');
-        pageNumber = pageNumber < pageContents.length - 2 ? ++pageNumber : 0;
-        document.getElementById(pageContents[pageNumber]).classList.remove('hide');
-
-
+        pages[pageNumber].classList.add('hide');
+        pageIndicator[pageNumber].classList.remove('bar-item--active');
+        pageNumber = ++pageNumber < pages.length ? pageNumber : 0;
+        pages[pageNumber].classList.remove('hide');
+        pageIndicator[pageNumber].classList.add('bar-item--active');
+        if(pageNumber === 0)
+            pageBack.classList.add('hide');
+        else if(pageNumber === 1){
+            pageBack.classList.remove('hide')
+        }
 
     });
 
-
-
+    pageBack.addEventListener('click', function () {
+        pages[pageNumber].classList.add('hide');
+        pageIndicator[pageNumber].classList.remove('bar-item--active');
+        pageNumber = pageNumber !== 0 ? --pageNumber : pages.length - 1;
+        pages[pageNumber].classList.remove('hide');
+        pageIndicator[pageNumber].classList.add('bar-item--active');
+        if(pageNumber === 0)
+            pageBack.classList.add('hide');
+        else if(pageNumber === 1){
+            pageBack.classList.remove('hide')
+        }
+    });
 });
